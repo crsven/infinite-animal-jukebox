e079ba84a01ede8b409690a6ea1e1cb44998d34f
Chris Svenningsen <chris@carbonfive.com>
2015-08-10 09:45:21 -0700

    Replace finished audio.

--- js/components/animal_audioscape.jsx
+++ js/components/animal_audioscape.jsx
@@ -6,12 +6,16 @@ var AnimalAudioscape = React.createClass({
     return (Math.random() * (max - min) + min);
   },
 
+  _generateAnimalId: function() {
+    return Math.floor(this._randFrom(10000, 130000));
+  },
+
   getInitialState: function() {
     var animalIds = [];
     while(animalIds.length < 5) {
-      var newRand = Math.floor(this._randFrom(10000, 130000));
-      if(animalIds.indexOf(newRand) < 0) {
-        animalIds.push(newRand);
+      var newAnimal = this._generateAnimalId();
+      if(animalIds.indexOf(newAnimal) < 0) {
+        animalIds.push(newAnimal);
       }
     };
     return {
@@ -22,18 +26,69 @@ var AnimalAudioscape = React.createClass({
   componentDidMount: function() {
     this._nodesReady = 0;
     Object.keys(this.refs).forEach(function(refKey) {
-      React.findDOMNode(this.refs[refKey]).addEventListener('canplay', function() {
+      var node = React.findDOMNode(this.refs[refKey]);
+      var intKey = parseInt(refKey);
+      node.addEventListener('canplay', function() {
         this._nodesReady++;
-        this._beginPlaying();
+        var refKeys = Object.keys(this.refs);
+        if(refKeys.length <= this._nodesReady) {
+          this._beginPlaying();
+        }
+      }.bind(this));
+      node.addEventListener('ended', function() {
+        this._removeAndReplace(intKey);
+      }.bind(this));
+      node.addEventListener('error', function() {
+        this._removeAndReplace(intKey);
+      }.bind(this));
+    }.bind(this));
+  },
+
+  _removeAndReplace: function(refKey) {
+    var animalIds = [].concat(this.state.animalIds);
+
+    var oldIndex = animalIds.indexOf(refKey);
+    animalIds.splice(oldIndex,1);
+
+    var oldNode = React.findDOMNode(this.refs[refKey]);
+    oldNode.removeEventListener('canplay');
+    oldNode.removeEventListener('ended');
+
+    var notFound = true;
+    while(notFound) {
+      var newAnimal = this._generateAnimalId();
+      if(animalIds.indexOf(newAnimal) < 0) {
+        animalIds.push(newAnimal);
+        notFound = false;
+      }
+    }
+
+    this.setState({
+      animalIds: animalIds
+    });
+  },
+
+  componentDidUpdate: function(prevProps, prevState) {
+    var newAnimals = this.state.animalIds.filter(function(animal) {
+      return prevState.animalIds.indexOf(animal) === -1;
+    });
+    newAnimals.forEach(function(animal) {
+      var node = React.findDOMNode(this.refs[animal]);
+      node.addEventListener('canplay', function() {
+        node.currentTime = 7;
+        node.play();
+      });
+      node.addEventListener('ended', function() {
+        this._removeAndReplace(animal);
+      }.bind(this));
+      node.addEventListener('error', function() {
+        this._removeAndReplace(animal);
       }.bind(this));
     }.bind(this));
   },
 
   _beginPlaying: function() {
-    var refKeys = Object.keys(this.refs);
-    if(refKeys.length !== this._nodesReady) return;
-
-    refKeys.forEach(function(refKey) {
+    Object.keys(this.refs).forEach(function(refKey) {
       var node = React.findDOMNode(this.refs[refKey]);
       if(node.played.length === 0) {
         node.currentTime = 7;
@@ -45,15 +100,24 @@ var AnimalAudioscape = React.createClass({
   render: function() {
     return(
       <div>
-        <h3>What if...</h3>
+        <h1>Infinite Animals</h1>
+        <p>
+          An infinite jukebox of animal recordings from the
+          <a href="http://macaulaylibrary.org/">Macauley Library</a>
+        </p>
+        <h3>Now listening to:</h3>
         {
           this.state.animalIds.map(function(animalId) {
-            return <Animal ref={ "audio-" + animalId }
+            return <a style={{display: "block", margin: "0 0 15px"}} href={"http://macaulaylibrary.org/audio/"+animalId}>{ animalId }</a>
+          })
+        }
+        {
+          this.state.animalIds.map(function(animalId) {
+            return <Animal ref={ animalId }
                            key={ animalId }
                            animalId={ animalId } />
           })
         }
-        <h3>lived together.</h3>
       </div>
     );
   }
